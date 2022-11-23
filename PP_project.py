b1819545129e2612544ca6788555b925e85504df
ayushdwivedi1375 <102732640+ayushdwivedi1375@users.noreply.github.com>
2022-11-23 15:52:17 +0530

    Update PP_project.py

--- PP_project.py
+++ PP_project.py
@@ -4,6 +4,8 @@ Created on Mon Nov 21 13:39:04 2022
 
 @author: hp
 """
+
+#Importing all the required libraries.
 import numpy as np
 from random import random
 import pandas as pd
@@ -11,18 +13,18 @@ from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 
-
+#Defining the activation function.
 def _sigmoid(x):
 
     y = 1.0 / (1 + np.exp(-x))
     return y
-
+# Defining the mean square error function.
 def _mse(target, output):
     return np.average((target - output) ** 2)
 
-
+#Implementing the exception handling.
 try:
-    df=pd.read_csv("C:\\Users\hp\Downloads\Sonar221.csv")
+    df=pd.read_csv("C:\\Users\hp\Downloads\Sonar221.csv")  #Reading the csv file.
     y1=df.loc[:,'label']
     y2=[]
     for i in y1:
@@ -34,8 +36,12 @@ try:
     xp=np.array(df) 
     x, x_test, y, y_test = train_test_split(xp, yl, train_size=0.8)
     m,n=x.shape
+    
+    #creating the neural network with two layers where n depends on number of features in the dataset.
     network=[n,5,1]
-
+    
+    
+    # Randomly assigning the weights and bias to the neural network.
     weights = []
     bias=[]
     for i in range(len(network) - 1):
@@ -43,11 +49,14 @@ try:
         b= np.random.rand(network[i+1],1)
         weights.append(w)
         bias.append(b)
-
+    #Defining the epochs and learning rate.
     epochs=50
     lr=0.1
     e=list(np.arange(epochs))
     L=[]
+    
+    
+    #Iterating over the epochs.
     for i in range(epochs):
         sum_errors = 0
 
@@ -58,8 +67,11 @@ try:
                 l=[]
                 l.append(k)
                 inp.append(l)
-            input=np.array(inp)    
-                
+            input=np.array(inp) 
+            
+            
+            
+            # feedforwarding our neural network with our iniatially defined random values of weights and bias.    
             y1 = y[j]
             z1=np.matmul(np.transpose(weights[0]),input)+bias[0]
             a1= _sigmoid(z1)
@@ -73,6 +85,10 @@ try:
             z2=np.matmul(np.transpose(weights[1]),a1)+bias[1]
             a2= _sigmoid(z2)
             error2= y1-a2
+            
+            
+            # Backpropagating the error for the first forward pass.
+            #Derivating the loss function w.r.t. weights and bias.
             der_a2=np.matmul(a2,np.transpose(np.ones(len(a2))-a2))
             der_L_w2=np.matmul(np.transpose(error2),np.matmul(der_a2,a1.T))
             der_L_b2=np.matmul(error2,der_a2)
@@ -81,8 +97,11 @@ try:
             der_L_w1=np.matmul(error1.T,np.matmul(a1.T,np.matmul((one-a1),input.T)))
             der_L_b1=np.matmul(error1,der_a1)
             
-            weights[1]=weights[1]+lr*(der_L_w2.T)
             
+            
+            
+            #Updating the weights and bias according to the values of derivatives we have got.
+            weights[1]=weights[1]+lr*(der_L_w2.T)
             weights[0]=weights[0]+lr*(der_L_w1.T)
             
             
@@ -93,7 +112,10 @@ try:
             
         print("Error: {} at epoch {}".format(sum_errors / len(x), i+1))
         L.append(sum_errors / len(x))
-        
+   
+
+
+    #plot between the mean square error and the number of epochs.
     plt.plot(e,L)
     plt.show()    
         
@@ -123,7 +145,7 @@ try:
         else:
             cal.append(0)
             
-        
+    # calculating our Accuracy for the tested data.   
     from sklearn.metrics import accuracy_score
     A=accuracy_score(y_test, cal)
     print(A)
